6879c20d9b965881641115e04dc8aaf3552845c8
Odroj <davisjordyn06@gmail.com>
2025-06-28 23:37:12 -0500

    Big Chnages to the OG File

--- bread.c
+++ bread.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main(){
+    int bread =0;
     printf("\nThis is practicing with git");
+    printf("My name is Jordyn-Rhys Davis and Im a student at Utech"); 
+    for(int f=0; f<3; f++){
+        bread += 1;
+    }
+    printf("Come and have a bread"); 
+    printf("I have %d bread", bread);
     return 0; 
 }
